73360126835a9f44a5845d1dfae4326a98ef7004
vhoang-vh <truc.51ksub@gmail.com>
2026-02-03 08:31:15 +0700

    Delete EmailService.java

--- EmailService.java
+++
@@ -1,9 +0,0 @@
-package di;
-
-public class EmailService implements MessageService {
-
-    @Override
-    public void sendMessage(String message) {
-        System.out.println("Email: " + message);
-    }
-}
